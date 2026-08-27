e2320f7720d8b56015d37f95291adfbc1cf372b7
CharlesVasseur <charles.vasseur@protonmail.com>
2026-08-27 16:09:46 +0200

    Computed daily loss ratio, as weekly data was not suitable for wars that lasted for less than a week (e.g. Bloodbath of B-R5RB)

--- R/loss_ratio_model.R
+++ R/loss_ratio_model.R
@@ -1,5 +1,7 @@
 source("R/load_data.R")
 
+### Weekly Loss Ratio
+
 aggregate_weekly <- function(hull_values) {
   hv <- as.data.table(hull_values)
   hv[, week_start := floor_date(date, unit = "week", week_start = 6)]
@@ -24,3 +26,25 @@ loss_ratio_b <- loss_ratio_b[!is.na(loss_ratio)]
 
 fwrite(loss_ratio_a, "data/aggregated/loss_ratio_window_a.csv")
 fwrite(loss_ratio_b, "data/aggregated/loss_ratio_window_b.csv")
+
+### Daily Loss Ratio
+
+aggregate_daily <- function(hull_values) {
+  hv <- as.data.table(hull_values)
+  hv[, .(total_payout = sum(estimated_hull_value)), by = date]
+}
+
+compute_loss_ratio_daily <- function(daily, window_days = 84) {
+  setorder(daily, date)
+  daily[, premium := shift(frollmean(total_payout, n = window_days, align = "right"), n = 1)]
+  daily[, loss_ratio := total_payout / premium]
+  daily
+}
+
+daily_a <- aggregate_daily(hull_value_window_a)
+daily_b <- aggregate_daily(hull_value_window_b)
+loss_ratio_daily_a <- compute_loss_ratio_daily(daily_a)[!is.na(loss_ratio)]
+loss_ratio_daily_b <- compute_loss_ratio_daily(daily_b)[!is.na(loss_ratio)]
+
+fwrite(loss_ratio_daily_a, "data/aggregated/loss_ratio_daily_window_a.csv")
+fwrite(loss_ratio_daily_b, "data/aggregated/loss_ratio_daily_window_b.csv")
